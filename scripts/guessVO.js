4bea626144dce1a32fc898a9860f5875aae357b5
mariyamikhailovna <itszemoff@gmail.com>
2026-04-21 18:21:48 +0800

    update to guessing game, v1.1

--- scripts/guessVO.js
+++ scripts/guessVO.js
@@ -1,4 +1,5 @@
 const frameContainer = document.querySelector(".frameContainer");
+
 let vos = [];
 let streak = 0;
 let highestStreak = 0;
@@ -20,15 +21,31 @@ function loadQuestion() {
     const randomIndex = Math.floor(Math.random() * vos.length);
     answer = vos[randomIndex].name;
     audio = new Audio(vos[randomIndex].audio);
+
+    audio.onended = () => {
+        document.querySelector(".btnPlay").textContent = "▶ Play";
+    };
 }
 
 function playVO() {
-   audio.play();
+    if(audio.paused){
+        audio.play();
+        document.querySelector(".btnPlay").textContent = "⏹ Stop";
+    }
+    else{
+        audio.pause();
+        audio.currentTime = 0;
+        document.querySelector(".btnPlay").textContent = "▶ Play";
+    }
 }
 
 function submit(){
     const input = document.querySelector(".input");
     const guess = input.value.trim().toLowerCase();
+
+    audio.pause();
+    audio.currentTime = 0;
+    document.querySelector(".btnPlay").textContent = "▶ Play";
 
      if (guess == answer.toLowerCase()) {
         streak++;
@@ -37,13 +54,33 @@ function submit(){
             highestStreak = streak;
             document.querySelector(".highstreak").textContent = highestStreak;
         }
-        alert("temporary correct notif because im stupid rn!");
+        alert("Correct!");
+        input.value = '';
+        loadQuestion();
     } else {
+        
         streak = 0;
         document.querySelector(".streak").textContent = streak;
-        alert(`u stupid! It was ${answer}.`);
-    }
 
-    input.value = '';
+        if(guess.length > 0){
+            alert(`${input.value} is Wrong! Try again.`);
+        }
+        else{
+            alert(`Please enter a guess!`);
+        }
+
+    }
+}
+
+function giveUp() {
+
+    audio.pause();
+    audio.currentTime = 0;
+    document.querySelector(".btnPlay").textContent = "▶ Play";
+
+    alert(`Too bad! The correct answer was: ${answer}`);
+    streak = 0;
+    document.querySelector(".streak").textContent = streak;
+
     loadQuestion();
 }
