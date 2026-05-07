189d4f3392cd234392b2b8724a859f762835b972
Jaime Labana Rodrigo <jaimelabana@gmail.com>
2026-05-07 21:39:18 +0800

    Update hero-list.js

--- scripts/hero-list.js
+++ scripts/hero-list.js
@@ -166,7 +166,7 @@ function render() {
 
     card.innerHTML = 
     `
-     <img src="wooperu/Land-Of-Dawn-Codex/blob/main/images/heroes/${hero.name.toLowerCase()}.png" alt="${hero.name}">
+     <img src="Land-Of-Dawn-Codex/blob/main/images/heroes/${hero.name.toLowerCase()}.png" alt="${hero.name}">
      <p class="hero-name">${hero.name}</p>
     `
     container.append(card);
