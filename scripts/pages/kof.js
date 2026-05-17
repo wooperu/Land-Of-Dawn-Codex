8aa69f3012002c13b112e10c9d223eb5b42d521e
cloud <ckgbasilio@gmail.com>
2026-05-17 15:14:55 +0800

    generate kof skins

---
+++ scripts/pages/kof.js
@@ -0,0 +1,6 @@
+renderSkinsFromFile("../../data/collab/kof.json", {
+  basePath  : "../../images/pages/skins-page/skins/portrait/",
+  category  : "collab",
+  subfolder : "kof",
+  gridId    : "portraitGrid"
+});
