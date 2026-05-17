1afde18328444e5b046a5944a9a77339dc77deeb
cloud <ckgbasilio@gmail.com>
2026-05-17 15:50:28 +0800

    render jjk skins and aot skins

---
+++ scripts/pages/aot.js
@@ -0,0 +1,6 @@
+renderSkinsFromFile("../../data/collab/aot.json", {
+  basePath  : "../../images/pages/skins-page/skins/portrait/",
+  category  : "collab",
+  subfolder : "aot",
+  gridId    : "portraitGrid"
+});
---
+++ scripts/pages/jjk.js
@@ -0,0 +1,6 @@
+renderSkinsFromFile("../../data/collab/jjk.json", {
+  basePath  : "../../images/pages/skins-page/skins/portrait/",
+  category  : "collab",
+  subfolder : "jjk",
+  gridId    : "portraitGrid"
+});
