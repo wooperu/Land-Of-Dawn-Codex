fb632c9020af445bb5f0a308900c4586a1e51f76
cloud <ckgbasilio@gmail.com>
2026-05-17 15:44:33 +0800

    render kung-fu-panda skins

---
+++ scripts/pages/kung-fu-panda.js
@@ -0,0 +1,6 @@
+renderSkinsFromFile("../../data/collab/kung-fu-panda.json", {
+  basePath  : "../../images/pages/skins-page/skins/portrait/",
+  category  : "collab",
+  subfolder : "kung-fu-panda",
+  gridId    : "portraitGrid"
+});
