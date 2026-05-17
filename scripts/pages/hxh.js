838a1f517779f8843573a7e8f16a9c0e56a8ee5a
cloud <ckgbasilio@gmail.com>
2026-05-17 15:51:26 +0800

    render hunter x hunter skins

---
+++ scripts/pages/hxh.js
@@ -0,0 +1,6 @@
+renderSkinsFromFile("../../data/collab/hxh.json", {
+  basePath  : "../../images/pages/skins-page/skins/portrait/",
+  category  : "collab",
+  subfolder : "hxh",
+  gridId    : "portraitGrid"
+});
