509392e79a07a3b803b1d4df32c7e79fb0fbfc77
cloud <ckgbasilio@gmail.com>
2026-05-20 00:57:58 +0800

    add: legend, mythic, collector, starlight pages

---
+++ scripts/pages/starlight.js
@@ -0,0 +1,9 @@
+initTabs("../data/skins/starlight.json", {
+  basePath      : "../images/pages/skins-page/skins/portrait/",
+  category      : "starlight",
+  tabId         : "tabBar",
+  gridId        : "portraitGrid",
+  skeletonCount : 3,
+  onRender      : function () { initReveal(); },
+  onEmpty       : function (grid) { grid.innerHTML = ""; }
+});
